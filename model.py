e2764247035eb277aa7ab9138a42767639c2867e
itsvivekghosh <soapmactevis1@gmail.com>
2020-06-18 19:28:34 +0000

    Added index.html, fixed bugs in camera.py and model.py file

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
-from tensorflow.keras import model_from_json
+from tensorflow.keras.models import model_from_json
 import numpy as np
 import tensorflow as tf
 
-config = tf.config.v1.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.15
-session = tf.compt.v1.Session(config = config)
+session = tf.compat.v1.Session(config = config)
 
 
 class FacialExpressionModel(object):
@@ -18,7 +18,7 @@ class FacialExpressionModel(object):
     def __init__(self, json_file, weights_file):
         with open(json_file, "r") as file:
             loaded_model = file.read()
-            self.loaded = model_from_json(loaded_model)
+            self.loaded_model = model_from_json(loaded_model)
 
         self.loaded_model.load_weights(weights_file)
         self.loaded_model._make_predict_function()
